72ac6a81405f657d2bd2555209bc0c65f7732a0f
SuperWizard7 <35956130+SuperWizard7@users.noreply.github.com>
2020-01-08 11:15:53 +0530

    Add files via upload

---
+++ AyurvedaQuestionnaire/app/src/main/java/com/ayurmanaha/ayurvedaquiz/questionViewModel.java
@@ -0,0 +1,21 @@
+package com.ayurmanaha.ayurvedaquiz;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.ViewModel;
+import java.util.ArrayList;
+
+public class questionViewModel extends AndroidViewModel{
+
+    public questionViewModel(Application application)
+    {
+        super(application);
+    }
+//    public ArrayList<Question> getQuestionInformation()
+//    {
+//        //QuizDbHelper dbHelper = new QuizDbHelper(this);
+//        //ArrayList<Question> qList = dbHelper.getAllQuestions();
+//    }
+    
+}
